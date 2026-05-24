7b7cc0b3037d58506017021cbd23bd643483e8c5
SpookyDervish <nchand05@outlook.com>
2026-05-25 07:59:21 +1000

    started work on compiling for the vm

--- src/compiler_vm.c
+++ src/compiler_vm.c
@@ -1,8 +1,60 @@
 #include "compiler_vm.h"
+#include "ast.h"
+#include "lexer.h"
+#include <stdlib.h>
 
-CometCVM* createCompilerVM(CometASTNode* ast) {
-    CometCVM* newVm = calloc(sizeof(CometCVM), 1);
-    newVm->ast = ast;
 
-    return newVm;
+// -- HELPER METHODS -- //
+CometOperand createOperand(CometOperandKind type) {
+    return (CometOperand){
+        .type = type
+    };
+}
+
+ResultType(CometOperand, charptr) visitInfixExpression(CometCompiler* c, CometASTNode* node);
+ResultType(CometOperand, charptr) resolveValue(CometCompiler* c, CometASTNode* node) {
+    switch (node->nodeType) {
+        case AST_INFIX_EXPRESSION:
+            return visitInfixExpression(c, node);
+
+        case AST_INT:
+            CometOperand new = createOperand(CO_CONST);
+            
+    }
+}
+
+// -- VISIT METHODS -- //
+ResultType(voidPtr, charptr) visitExpressionStatement(CometCompiler* c, CometASTNode* node) {
+    return compile(c, node->data.AST_EXPRESSION_STATEMENT.expression);
+}
+
+ResultType(CometOperand, charptr) visitInfixExpression(CometCompiler* c, CometASTNode* node) {
+    ResultType(CometOperand, charptr) left = resolveValue(c, node);
+}
+
+// -- MAIN -- //
+CometCompiler* createCompilerVM() {
+    CometCompiler* newCompiler = calloc(sizeof(CometCompiler), 1);
+    return newCompiler;
+}
+
+ResultType(voidPtr, charptr) compile(CometCompiler* c, CometASTNode* node) {
+    switch (node->nodeType) {
+        case AST_EXPRESSION_STATEMENT:
+            return visitExpressionStatement(c, node);
+
+        case AST_INFIX_EXPRESSION:
+            ResultType(CometOperand, charptr) value = visitInfixExpression(c, node);
+            if (value.error) {
+                return Error(voidPtr, charptr, value.as.error);
+            }
+
+        default: {
+            Estr errMsg = CREATE_ESTR("No compiler visit method for \"");
+            APPEND_ESTR(errMsg, ASTNodeTypeToCStr(node->nodeType));
+            APPEND_ESTR(errMsg, "\"!");
+
+            return Error(voidPtr, charptr, errMsg.str);
+        }
+    }
 }
--- src/compiler_vm.h
+++ src/compiler_vm.h
@@ -2,31 +2,63 @@
 #define VM_H
 
 #include "ast.h"
+#include "lexer.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "../include/error.h"
 
 typedef enum {
-    CVT_VOID,
-    CVT_SMALL,
-    CVT_INT,
-    CVT_BIG,
-    CVT_FLOAT,
-    CVT_DOUBLE,
-    CVT_BOOL
-} CometValueType;
+    COMET_VOID,
+    COMET_SMALL,
+    COMET_INT,
+    COMET_BIG,
+    COMET_FLOAT,
+    COMET_DOUBLE,
+    COMET_BOOL
+} CometValueTypeKind;
+
+typedef enum {
+    CO_REG,
+    CO_CONST
+} CometOperandKind;
 
 typedef struct {
-    CometValueType type;
+    CometValueTypeKind typeKind;
     union {
-        char smallValue;
-        int intValue;
-        long long bigValue;
-        float floatValue;
-        double doubleValue;
-        bool boolValue;
+        int8_t smallVal;
+        int32_t intVal;
+        int64_t bigVal;
+        float floatVal;
+        double doubleVal;
+        bool boolVal;
     };
-} CometValue;
+} CometConstant;
+
+typedef struct {
+    CometOperandKind type;
+    union {
+        uint32_t reg;
+        uint32_t constIdx;
+    };
+} CometOperand;
+
+typedef enum {
+    MOV,
+    ADD
+} CometInstType;
+
+typedef struct {
+    CometInstType opcode;
+    CometOperand dest;
+    CometOperand a;
+    CometOperand b;
+} CometInst;
+
+typedef struct {
+    CometValueTypeKind inferredType;
+    bool alive;
+} CometTemp;
 
 /*
 Not the actual VM that runs the code.
@@ -34,8 +66,17 @@ Not the actual VM that runs the code.
 This struct is used for register allocation and generation of instructions.
 */
 typedef struct {
-    CometValue registers[256];
-    CometASTNode* ast;
-} CometCVM;
+    CometTemp temps[256];
+    CometInst outputProgram[16];
+    CometConstant consts[256];
+} CometCompiler;
+
+typedef void* voidPtr;
+
+Result(voidPtr, charptr);
+Result(CometOperand, charptr);
+
+ResultType(voidPtr, charptr) compile(CometCompiler* c, CometASTNode* node);
+CometOperand createOperand(CometOperandKind type);
 
 #endif
