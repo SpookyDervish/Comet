db272c94b7b691e4d23e83bcc1b561ff0d15f296
SpookyDervish <nchand05@outlook.com>
2026-06-08 21:38:04 +1000

    remove uneeded print from test external lib

--- io/src/main.c
+++ io/src/main.c
@@ -1,8 +1,6 @@
 #include <comet/cometlib.h>
-#include <stdio.h>
 
 CometOperand impl_subtract(CometOperand* args, CometVM* vm) {
-    printf("we loaded a function and called it!\n");
     return cometValue(COMET_INT, args[0].imm.intVal - args[1].imm.intVal);
 }
 
