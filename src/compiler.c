ccd6fd314eb0fcd8c3595bcbaa254b3370e163b5
SpookyDervish <nchand05@outlook.com>
2026-07-05 13:26:47 +1000

    let methods be inline

--- src/compiler.c
+++ src/compiler.c
@@ -3623,10 +3623,20 @@ ResultType(CometOperand, ErrorMessage) visitMethodDefStatement(CometCompiler* c,
         false
     );
 
-    // build the functions body
-    ResultType(CometOperand, ErrorMessage) bodyResult = compile(c, funcDef.program);
-    if (bodyResult.error)
-        return bodyResult;
+    if (funcDef.inlineExpr != NULL) { // its an inline function
+        // build the functions body
+        ResultType(CometOperand, ErrorMessage) exprResult = compile(c, funcDef.inlineExpr);
+        if (exprResult.error)
+            return exprResult;
+
+        // then return
+        buildReturn(c);
+    } else {
+        // build the functions body
+        ResultType(CometOperand, ErrorMessage) bodyResult = compile(c, funcDef.program);
+        if (bodyResult.error)
+            return bodyResult;
+    }
 
     // return back to the parent scope
     c->env = destroyEnv(c->env);
